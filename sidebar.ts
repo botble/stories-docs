40da76f42a5d505699302fdb6c55cf7953fb1ec3
Sang Nguyen <sangnguyenplus@gmail.com>
2023-08-16 14:57:57 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -26,12 +26,17 @@ export default [
   {
     text: 'Usage',
     items: [
-      { text: 'Setup email', link: '/stories/usage-email' },
+      { text: 'Rename theme', link: '/stories/theme-rename' },
+      { text: 'Custom CSS/JS', link: '/stories/usage-custom-css-js' },
+      { text: 'Translation', link: '/stories/usage-translation' },
+      { text: 'Multi-language', link: '/stories/usage-multi-language' },
+      { text: 'Location', link: '/stories/usage-location' },
+      { text: 'Google Analytics', link: '/stories/usage-analytics' },
       { text: 'Media - Setup Amazon S3', link: '/stories/usage-media-s3' },
       { text: 'Media - Setup BunnyCDN', link: '/stories/usage-media-bunnycdn' },
       { text: 'Media - Setup Wasabi', link: '/stories/usage-media-wasabi' },
-      { text: 'Analytics', link: '/stories/usage-analytics' },
-      { text: 'Rename theme', link: '/stories/theme-rename' },
+      { text: 'Setup email', link: '/stories/usage-email' },
+      { text: 'Backup', link: '/stories/plugin-backup' },
     ],
   },
 ] satisfies DefaultTheme.SidebarItem[];
