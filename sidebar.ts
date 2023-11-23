81373ff01549931449963cddfab8c02f10405d71
Sang Nguyen <sangnguyenplus@gmail.com>
2023-11-23 17:43:11 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -18,25 +18,17 @@ export default [
     ],
   },
   {
-    text: 'Available plugins',
-    items: [
-      { text: 'Backup', link: '/stories/plugin-backup' },
-    ],
-  },
-  {
     text: 'Usage',
     items: [
       { text: 'Rename theme', link: '/stories/theme-rename' },
       { text: 'Custom CSS/JS', link: '/stories/usage-custom-css-js' },
       { text: 'Translation', link: '/stories/usage-translation' },
       { text: 'Multi-language', link: '/stories/usage-multi-language' },
-      { text: 'Location', link: '/stories/usage-location' },
       { text: 'Google Analytics', link: '/stories/usage-analytics' },
       { text: 'Media - Setup Amazon S3', link: '/stories/usage-media-s3' },
       { text: 'Media - Setup BunnyCDN', link: '/stories/usage-media-bunnycdn' },
       { text: 'Media - Setup Wasabi', link: '/stories/usage-media-wasabi' },
       { text: 'Setup email', link: '/stories/usage-email' },
-      { text: 'Backup', link: '/stories/plugin-backup' },
     ],
   },
 ] satisfies DefaultTheme.SidebarItem[];
